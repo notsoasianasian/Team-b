4e9bd0b93c54d35889cbb36874ead068935585b5
notsoasianasian <44407935+notsoasianasian@users.noreply.github.com>
2019-04-18 06:04:14 -0400

    Initial commit

---
+++ Assets/Scripts/Objects/GemScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemScript : MonoBehaviour
+{
+   // public GameObject Cloud;
+
+    // Use this for initialization
+    void Start()
+    {
+        //  audio = GameObject.Find("gem").GetComponent<AudioSource>();
+
+        if (Door.instance != null)
+        {
+            Door.instance.collectiblesCount++;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+
+
+       
+
+        if (target.tag == "Player")
+        {
+           // Instantiate(Cloud, transform.position, Cloud.transform.rotation);
+
+            Destroy(gameObject);
+
+            if (Door.instance != null)
+            {
+                Door.instance.DecrementCollectables();
+            }
+        }
+    }
+
+}
